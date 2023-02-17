e2ff3fb66d5b6036d3f1c00b0549a989d9d0330e
kcx1 <casey@ascendstudios.com>
2023-02-17 17:25:30 -0500

    Enabling system link

--- FileCleanup/symlink_config.py
+++ FileCleanup/symlink_config.py
@@ -1,15 +1,27 @@
 from pathlib import Path
-from file_cleanup import normalize
-from FileCleanup.config import config
+import os
+from FileCleanup.file_cleanup import normalize, configuration
+
 
 # Path to installed config
 config_file = Path("config/config.toml").absolute()
 # Path declared in config
-symlink = normalize(config["Config"]["location"]).absolute()
+symlink_dir = normalize(configuration["location"])
+symlink = symlink_dir.joinpath("config.toml").absolute()
 
 
 def main():
-    config_file.symlink_to(symlink, True)
+    if not symlink_dir.is_dir() and not symlink_dir.exists():
+        symlink_dir.mkdir()
+    elif not symlink_dir.is_dir() and symlink_dir.exists():
+        SystemExit("Configuration location should be a directory not a file or symlink")
+    elif symlink.exists():
+        SystemExit("Config File Already Exisits")
+
+    if symlink.is_absolute() and config_file.is_absolute():
+        print(Path.cwd(), symlink, config_file)
+        symlink.symlink_to(config_file, True)
+        # os.link(config_file.__str__(), symlink.__str__())
 
 
 if __name__ == "__main__":
